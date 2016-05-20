a540c2ba46d0ab997dd8163b29a9efe6487bddb4
agreementwow <agreementwow@gmail.com>
2016-05-20 18:00:48 +0800

    add Textfield Specifications

--- Meme 1.0/ViewController.swift
+++ Meme 1.0/ViewController.swift
@@ -8,15 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var camera: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var btmTextField: UITextField!
+    
+    //setting font style and color
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName : UIColor.greenColor(),
+        NSForegroundColorAttributeName : UIColor.blueColor(),
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName : 3.0
+    ]
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        topTextField.text = "TOP"
+        btmTextField.text = "BOTTOM"
+        topTextField.textAlignment = .Center
+        btmTextField.textAlignment = .Center
+        self.topTextField.delegate = self
+        self.btmTextField.delegate = self
+        topTextField.defaultTextAttributes = memeTextAttributes
+        btmTextField.defaultTextAttributes = memeTextAttributes
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -45,7 +64,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     //set the done and cancel button after picking an image
-    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = image
@@ -56,5 +74,25 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    //clear the default text when a user taps inside a textfield
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if topTextField.text == "TOP" {
+            topTextField.text = ""
+        }
+        else if btmTextField.text == "BOTTOM" {
+            btmTextField.text = ""
+        }
+    }
+    
+    //when a user presses return, the keyboard should be dismissed
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        topTextField.resignFirstResponder()
+        btmTextField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
 }
 
